39f42222eba41494e2d4b16d2e6c7dd9567619b4
jfkbeatkid <hernadezrigoberto@gmail.com>
2016-07-05 20:13:05 -0400

    shell of db setup

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 var express = require('express');
 var server = express();
+var bodyParser = require('body-parser');
+var lowdb = require('lowdb');
+var uuid = require('uuid');
+
+var port = process.env.PORT || 8080
+var db = lowdb('db.json');
+
+db.defaults({plants: []})
+  .value();
+
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({extened: true}));
+
+server.listen(port, function(){
+  console.log('Now listening to port:', port);
+});
