d9bc4c77b7e09b27981a5cf22eee9d4997517904
jfkbeatkid <hernadezrigoberto@gmail.com>
2016-07-05 20:54:24 -0400

    added all routes, all work in postman

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var server = express();
+var server = (express());
 var bodyParser = require('body-parser');
 var lowdb = require('lowdb');
 var uuid = require('uuid');
@@ -11,16 +11,64 @@ db.defaults({plants: []})
   .value();
 
 server.use(bodyParser.json());
-server.use(bodyParser.urlencoded({extened: true}));
+server.use(bodyParser.urlencoded({extended: true}));
+
+server.get('/plants', function(request, response){
+  var plants = db.get('plants')
+                .value();
+  response.send(plants);
+})
+
+server.get('/plants/:id', function(request, response){
+  var plant = db.get('plants')
+                .find({id: request.params.id})
+                .value()
+  response.send(plant);
+})
 
 server.post('/plants', function(request, response){
   var plant = {
     id: uuid.v4(),
     commonName: request.body.commonName,
     scientificName: request.body.scientificName,
-    typeOfPlant: request.body.typeOfPLant, 
-  }
+    typeOfPlant: request.body.typeOfPLant,
+    edible: false,
+    edibleParts: request.body.edibleParts,
+    medicinal: false,
+    medicinalParts: request.body.medicinalParts
+  };
+
+  var result = db.get('plants')
+                 .push(plant)
+                 .last()
+                 .value();
+  response.send(result);
 });
+
+server.put('/plants/:id', function(request, response){
+  var updatedPlantInfo = {
+    commonName: request.body.commonName,
+    scientificName: request.body.scientificName,
+    typeOfPlant: request.body.typeOfPLant,
+    edible: request.body.edible,
+    edibleParts: request.body.edibleParts,
+    medicinal: request.body.medicinal,
+    medicinalParts: request.body.medicinalParts
+  };
+
+  var updatedPlant = db.get('plants')
+                       .find({id: request.params.id})
+                       .assign(updatedPlantInfo)
+                       .value();
+  response.send(updatedPlant);
+})
+
+server.delete('/plants/:id', function(request, response){
+  var plant = db.get('plants')
+                .remove({id: request.params.id})
+                .value();
+  response.send(plant);
+})
 
 server.listen(port, function(){
   console.log('Now listening to port:', port);
